8d9c5db32706a7347897255af6693d4af65152bc
Hayden LeBaron <hlebaron98@gmail.com>
2020-09-27 15:49:18 -0600

    WIP

---
+++ location_entry.py
@@ -0,0 +1,5 @@
+class LocationEntry
+    def __init__(self, timestamp_ms, latitude_e7, longitude_e7,
+     accuracy velocity, heading, altitude, vertical_accuracy)
+        self.timestamp_ms = timestamp
+        self.latidue_e7
